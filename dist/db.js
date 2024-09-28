0ce4820145ec56b3aa230a288a6f269ed1fd9387
Tien Dat <dat25363@gmail.com>
2024-09-28 20:06:53 +0700

    env

--- dist/db.js
+++ dist/db.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql2_1 = __importDefault(require("mysql2"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Load environment variables from .env file
+dotenv_1.default.config();
 const db = mysql2_1.default.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456789',
-    database: 'BE2024_162',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 db.connect((err) => {
     if (err) {
