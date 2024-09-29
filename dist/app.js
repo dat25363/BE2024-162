56edae37953bdfc825a71f76b2f2ae93f3e02178
Tien Dat <dat25363@gmail.com>
2024-09-29 19:09:48 +0700

    swagger

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const keyword_search_1 = __importDefault(require("./keyword-search"));
+const product_search_1 = __importDefault(require("./product-search"));
 const cors_1 = __importDefault(require("cors"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const yamljs_1 = __importDefault(require("yamljs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+const swaggerDocument = yamljs_1.default.load(path_1.default.resolve(__dirname, "../swagger.yaml"));
 app.use((0, cors_1.default)());
-app.use('/api/keyword-search', keyword_search_1.default);
+app.use('/api/product-search', product_search_1.default);
+app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Swagger docs are available on http://localhost:${port}/api-docs`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
 import express from 'express'; 
-import keywordSearch from './keyword-search'; 
+import productSearch from './product-search'; 
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 
 const app = express();
+const swaggerDocument = YAML.load(path.resolve(__dirname, "../swagger.yaml"));
+
 app.use(cors());
 
-app.use('/api/keyword-search', keywordSearch);
+app.use('/api/product-search', productSearch);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Swagger docs are available on http://localhost:${port}/api-docs`);
 });
 
